21d54a442e738613aad7d231b6c3f6194a90c60a
Brian Gao <bhgao20014@gmail.com>
2021-08-09 18:16:22 -0400

    Refactoring code so that when a project is selected, the hover effect will stay; using js instead of plain css

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,28 @@ activateBtns();
 
 function activateBtns() {
   const projectHeaders = document.querySelectorAll(".project-main");
-  projectHeaders.forEach((project) => project.addEventListener("click", showContent), {capture: true})
+  projectHeaders.forEach((project) => {
+    project.addEventListener("click", showContent)
+    project.addEventListener("mouseenter", addHover)
+    project.addEventListener("mouseleave", removeHover)
+  })
+}
+
+function removeHover(e) {
+  const projectMain = e.path[0].classList[0] == "project-main" ? e.path[0] : e.path[1];
+  projectMain.classList.remove("project-main-hover");
+  projectMain.firstElementChild.classList.remove("dropdown-hover")
+
+
+
+}
+
+function addHover(e) {
+  const projectMain = e.path[0].classList[0] == "project-main" ? e.path[0] : e.path[1];
+  projectMain.classList.add("project-main-hover");
+  projectMain.firstElementChild.classList.add("dropdown-hover");
+  projectMain.secondElementChild.classList.add("project-header-dropdown")
+
 }
 
 function showContent(e) {
